ef9e6d58bdc2cf52c3e688cf3c2846da7cf03b6c
Krish Mittal <krishmittal212005@gmail.com>
2024-04-11 17:58:44 +0530

    Product Page Nav

---
+++ HouseRizz-iOS/View/ProductView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductView.swift
+//  HouseRizz-iOS
+//
+//  Created by Krish Mittal on 11/04/24.
+//
+
+import SwiftUI
+
+struct ProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductView()
+}
