0f5343849a6a299fb854575d003596e80e61dd82
Krish Mittal <krishmittal212005@gmail.com>
2024-04-12 07:39:22 +0530

    cart button

---
+++ HouseRizz-iOS/Components/CartButton.swift
@@ -0,0 +1,32 @@
+//
+//  CartButton.swift
+//  HouseRizz-iOS
+//
+//  Created by Krish Mittal on 12/04/24.
+//
+
+import SwiftUI
+
+struct CartButton: View {
+    var numberOfProducts: Int
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Image(systemName: "bag.fill")
+                .padding(5)
+            
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption2)
+                    .foregroundStyle(.white)
+                    .frame(width: 15, height: 15)
+                    .background(.purple)
+                    .cornerRadius(50)
+            }
+        }
+    }
+}
+
+#Preview {
+    CartButton(numberOfProducts: 1)
+}
