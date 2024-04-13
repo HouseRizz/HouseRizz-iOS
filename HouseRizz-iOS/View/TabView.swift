8f84d3839ce9c1c0c9c35c70e71ee49ca89b74f9
Krish Mittal <krishmittal212005@gmail.com>
2024-04-13 08:41:36 +0530

    icons

---
+++ HouseRizz-iOS/View/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  HouseRizz-iOS
+//
+//  Created by Krish Mittal on 13/04/24.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
