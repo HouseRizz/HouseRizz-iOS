b1c49357e1ca821c86c35aa0ef44ea5b751b3229
Krish Mittal <krishmittal212005@gmail.com>
2024-05-27 17:58:46 +0530

    CKCartViewModel

---
+++ HouseRizz-iOS/ViewModel/CKCartViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  CKCartViewModel.swift
+//  HouseRizz-iOS
+//
+//  Created by Krish Mittal on 27/05/24.
+//
+
+import Foundation
+
+class CKCartViewModel: ObservableObject {
+    @Published private(set) var products: [CKCartItem] = []
+    @Published private(set) var total: Int = 0
+
+    func addToCart(product: HRCKProduct, quantity: Int = 1) {
+        if let index = products.firstIndex(where: { $0.product.id == product.id }) {
+            products[index].quantity += quantity
+        } else {
+            let cartItem = CKCartItem(product: product, quantity: quantity)
+            products.append(cartItem)
+        }
+        total += (product.price ?? 0) * quantity
+    }
+
+    func removeFromCart(product: HRCKProduct) {
+        if let index = products.firstIndex(where: { $0.product.id == product.id }) {
+            let cartItem = products[index]
+            if cartItem.quantity > 1 {
+                products[index].quantity -= 1
+            } else {
+                products.remove(at: index)
+            }
+            total -= product.price ?? 0
+        }
+    }
+
+    func updateCartItemQuantity(cartItem: CKCartItem, newQuantity: Int) {
+        if let index = products.firstIndex(where: { $0.product.id == cartItem.product.id }) {
+            products[index].quantity = newQuantity
+            total = products.reduce(0) { $0 + ($1.quantity * ($1.product.price ?? 0)) }
+        }
+    }
+}
+
+struct CKCartItem {
+    let product: HRCKProduct
+    var quantity: Int
+}
