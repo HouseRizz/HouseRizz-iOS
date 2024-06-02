63c3f2c0f174c662d86d5d4dde59076019367251
Krish Mittal <krishmittal212005@gmail.com>
2024-06-02 07:51:26 +0530

    Handling Warnings

--- HouseRizz-iOS/Utilities/Extensions/ColorExtension.swift
+++ HouseRizz-iOS/Utilities/Extensions/ColorExtension.swift
@@ -8,6 +8,6 @@
 import SwiftUI
 
 extension Color {
-    static let primaryColor = Color("PrimaryColor")
-    static let secondaryColor = Color("SecondaryColor")
+    static let primaryColor = Color("BrandOrange")
+    static let secondaryColor = Color("BrandOrangeSecondary")
 }
