94746918651714cc9a014720ff9b7fff272c530d
Krish Mittal <krishmittal212005@gmail.com>
2024-06-12 20:30:06 +0530

    searching

--- HouseRizz-iOS/View/Buyer/Common/SearchView.swift
+++ HouseRizz-iOS/View/Buyer/Common/SearchView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct SearchView: View {
+    @StateObject private var viewModel = SearchViewModel()
+    @EnvironmentObject var cartViewModel: CartViewModel
+    @State private var selectedProduct: HRProduct?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
+                        ForEach(viewModel.filteredProducts, id: \.self) { product in
+                            ProductCardView(product: product)
+                                .environmentObject(cartViewModel)
+                                .onTapGesture {
+                                    selectedProduct = product
+                                }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
+            .refreshable {
+                viewModel.fetchItems()
+            }
+            .navigationTitle("")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                ToolbarItem {
+                    HStack {
+                        HStack {
+                            TextField("Search For Your Products", text: $viewModel.search)
+                                .padding()
+                            
+                            Image(systemName: "magnifyingglass")
+                                .padding(.trailing)
+                            
+                        }
+                        .background(Color.primaryColor.opacity(0.2))
+                        .cornerRadius(20)
+                    }
+                }
+            })
+            .environmentObject(cartViewModel)
+            .sheet(item: $selectedProduct) { product in
+                ProductDetailsView(product: product)
+                    .environmentObject(cartViewModel)
+            }
+        }
     }
-}
-
-#Preview {
-    SearchView()
 }
