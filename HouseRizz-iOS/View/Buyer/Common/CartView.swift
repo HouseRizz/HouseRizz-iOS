eca97de0f59ca78428c99139e3441efc5e427029
Krish Mittal <krishmittal212005@gmail.com>
2024-06-15 15:22:17 +0530

    check before checking out whether the user has submitted address and phone number

--- HouseRizz-iOS/View/Buyer/Common/CartView.swift
+++ HouseRizz-iOS/View/Buyer/Common/CartView.swift
@@ -10,47 +10,74 @@ import SwiftUI
 struct CartView: View {
     @EnvironmentObject var cartViewModel: CartViewModel
     @StateObject private var authViewModel = Authentication()
-    @State private var showAlert = false
+    @State private var showAlert: Bool = false
+    @State private var navigateToUPIView: Bool = false
+    @State private var navigateToSetting: Bool = false
     
     var body: some View {
-        VStack {
-            if !cartViewModel.products.isEmpty {
-                ScrollView {
-                    ForEach(cartViewModel.products, id: \.product.id) { cartItem in
-                        CartProductView(cartItem: cartItem)
-                            .environmentObject(cartViewModel)
+        NavigationView {
+            VStack {
+                if !cartViewModel.products.isEmpty {
+                    ScrollView {
+                        ForEach(cartViewModel.products, id: \.product.id) { cartItem in
+                            CartProductView(cartItem: cartItem)
+                                .environmentObject(cartViewModel)
+                        }
                     }
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("Your Total is ")
+                        Spacer()
+                        Text((cartViewModel.total).formattedCurrency())
+                            .bold()
+                    }
+                    .padding()
+                    
+                    Button(action: {
+                        if authViewModel.user?.address != "Not Provided" && authViewModel.user?.phoneNumber != "Not Provided" {
+                            navigateToUPIView = true
+                        } else {
+                            showAlert = true
+                        }
+                    }) {
+                        Text("Proceed to Checkout")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.primaryColor)
+                            .cornerRadius(10)
+                            .padding()
+                    }
+                    .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("Address and Phone Missing"),
+                            message: Text("Please provide your address and phone number to proceed."),
+                            primaryButton: .default(Text("Go to Settings"), action: {
+                                navigateToSetting.toggle()
+                            }),
+                            secondaryButton: .cancel()
+                        )
+                    }
+                    .navigationDestination(isPresented: $navigateToUPIView) {
+                        UPIView()
+                    }
+                    .sheet(isPresented: $navigateToSetting) {
+                        SettingsView()
+                    }
+                } else {
+                    Text("Your Cart is Empty")
                 }
-                
-                Divider()
-                
-                HStack {
-                    Text("Your Total is ")
-                    Spacer()
-                    Text((cartViewModel.total).formattedCurrency())
-                        .bold()
-                }
-                .padding()
-                
-                NavigationLink(destination: UPIView()) {
-                    Text("Proceed to Checkout")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.primaryColor)
-                        .cornerRadius(10)
-                        .padding()
-                }
-            } else {
-                Text("Your Cart is Empty")
             }
-        }
-        .navigationTitle("My Cart")
-        .padding(.vertical)
-        .onAppear {
-            authViewModel.fetchUser()
+            .navigationTitle("My Cart")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.vertical)
+            .onAppear {
+                authViewModel.fetchUser()
+            }
         }
     }
 }
