20c6d4b7ff33dcab2c577231a58a5c8a0ee5300e
Krish Mittal <krishmittal212005@gmail.com>
2024-06-19 17:04:16 +0530

    Add Banner Management

--- HouseRizz-iOS/ObservableObject/ViewModel/Settings/Admin/ManageProductCategoriesViewModel.swift
+++ HouseRizz-iOS/ObservableObject/ViewModel/Settings/Admin/ManageProductCategoriesViewModel.swift
@@ -13,52 +13,13 @@ class ManageProductCategoriesViewModel: ObservableObject {
     @Published var error: String = ""
     @Published var permissionStatus: Bool = false
     @Published var categories: [HRProductCategory] = []
-    @Published var name: String = ""
     @Published var selectedPhotoData = [Data]()
     var cancellables = Set<AnyCancellable>()
-    var urls: [URL] = []
-    var errors: [Error] = []
     
     init(){
         getiCloudStatus()
         requestPermission()
         fetchCategories()
-    }
-    
-    func addButtonPressed(){
-        guard !name.isEmpty else {return}
-        addCategory(name: name)
-        
-    }
-    
-    func clearItem() {
-        error = ""
-        name = ""
-        selectedPhotoData = []
-    }
-    
-    private func addCategory(name: String) {
-        for (index, imageData) in selectedPhotoData.enumerated() {
-            guard let image = UIImage(data: imageData),
-                  let url = FileManager
-                                .default
-                                .urls(for: .cachesDirectory, in: .userDomainMask)
-                                .first?.appendingPathComponent("photo\(index + 1).jpg"),
-                  let data = image.jpegData(compressionQuality: 1.0) else { continue }
-            do {
-                try data.write(to: url)
-                urls.append(url)
-            } catch {
-                errors.append(error)
-            }
-        }
-        
-        guard let newCategory = HRProductCategory(id: UUID(), name: name, imageURL: urls[0] ) else {
-            error = "Error creating item"
-            return
-        }
-        
-        CKUtility.add(item: newCategory) { _ in }
     }
     
     private func getiCloudStatus(){
--- HouseRizz-iOS/View/Settings/Admin/ManageAddBannerView.swift
+++ HouseRizz-iOS/View/Settings/Admin/ManageAddBannerView.swift
@@ -9,9 +9,75 @@ import SwiftUI
 
 struct ManageAddBannerView: View {
     @StateObject private var viewModel = ManageAddBannerViewModel()
+    @State private var showDeleteOption: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                ScrollView {
+                    ForEach(viewModel.adds.indices, id: \.self) { index in
+                        let add = viewModel.adds[index]
+                        ZStack(alignment: .topTrailing) {
+                            VStack {
+                                if let url = add.imageURL, let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .frame(width: .none, height: 180)
+                                        .scaledToFit()
+                                        .cornerRadius(15)
+                                }
+                                
+                                HStack {
+                                    Text("\(add.name)")
+                                    Text("\(add.sliderNumber)")
+                                }
+                            }
+                            
+                            Image(systemName: "minus.circle.fill")
+                                .imageScale(.large)
+                                .bold()
+                                .foregroundStyle(.red)
+                                .opacity(showDeleteOption ? 1.0 : 0.0)
+                                .onTapGesture {
+                                    viewModel.deleteItem(indexSet: IndexSet(integer: index))
+                                }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .onAppear {
+                viewModel.fetchAddBanners()
+            }
+            .refreshable {
+                viewModel.fetchAddBanners()
+            }
+            .navigationTitle("Manage Add Banners")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        showDeleteOption.toggle()
+                    } label: {
+                        Image(systemName: showDeleteOption ? "minus.circle.fill": "minus.circle" )
+                            .imageScale(.large)
+                            .bold()
+                            .foregroundStyle(.red)
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        AddAddBannerView()
+                            .toolbarRole(.editor)
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .imageScale(.large)
+                            .bold()
+                    }
+                }
+            }
+        }
     }
 }
 
