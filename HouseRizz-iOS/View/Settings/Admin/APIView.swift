facf0315cd06e66ef9541e4490d13411ef91e547
Krish Mittal <krishmittal212005@gmail.com>
2024-06-20 15:48:19 +0530

    APIViewModel

---
+++ HouseRizz-iOS/View/Settings/Admin/APIView.swift
@@ -0,0 +1,18 @@
+//
+//  APIView.swift
+//  HouseRizz-iOS
+//
+//  Created by Krish Mittal on 20/06/24.
+//
+
+import SwiftUI
+
+struct APIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    APIView()
+}
