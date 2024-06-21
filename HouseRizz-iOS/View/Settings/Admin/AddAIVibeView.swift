61ab7d3a49325a18d7571f5fd19adc01f8ea0be7
Krish Mittal <krishmittal212005@gmail.com>
2024-06-21 15:28:12 +0530

    ManageAIVibeView

---
+++ HouseRizz-iOS/View/Settings/Admin/AddAIVibeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAIVibeView.swift
+//  HouseRizz-iOS
+//
+//  Created by Krish Mittal on 21/06/24.
+//
+
+import SwiftUI
+
+struct AddAIVibeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAIVibeView()
+}
--- HouseRizz-iOS/View/Settings/Admin/ManageAIVibeView.swift
+++ HouseRizz-iOS/View/Settings/Admin/ManageAIVibeView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct ManageAIVibeView: View {
+    @State private var viewModel = ManageAIVibeViewModel()
+    let columns = [GridItem(.flexible()), GridItem(.flexible()),GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(viewModel.vibes.indices, id: \.self) { category in
+//                            NavigationLink(destination: ProductCategoryView(productCategory: viewModel.categories[category])) {
+//                                CategoryCard(category: viewModel.categories[category])
+//                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .onAppear {
+                viewModel.fetchVibes()
+            }
+            .refreshable {
+                viewModel.fetchVibes()
+            }
+            .navigationTitle("Product Categories")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        AddAIVibeView()
+                            .toolbarRole(.editor)
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .imageScale(.large)
+                            .bold()
+                    }
+                }
+            }
+        }
     }
 }
 
