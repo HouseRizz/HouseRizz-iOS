952f5feb9d9252813fc452d5275a2fed8926468e
Krish Mittal <krishmittal212005@gmail.com>
2024-06-21 15:22:59 +0530

    ManageAIVibeViewModel

---
+++ HouseRizz-iOS/ObservableObject/ViewModel/Settings/Admin/ManageAIVibeViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ManageAIVibeViewModel.swift
+//  HouseRizz-iOS
+//
+//  Created by Krish Mittal on 21/06/24.
+//
+
+import Foundation
+import Combine
+
+@Observable
+class ManageAIVibeViewModel {
+    var error: String = ""
+    var vibes: [HRAIVibe] = []
+    var selectedPhotoData = [Data]()
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        fetchVibes()
+    }
+    
+    func fetchVibes(){
+        let predicate = NSPredicate(value: true)
+        let recordType = HRAIVibeModelName.itemRecord
+        CKUtility.fetch(predicate: predicate, recordType: recordType, sortDescription: [NSSortDescriptor(key: "name", ascending: true)])
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    self?.error = error.localizedDescription
+                }
+            } receiveValue: { [weak self] returnedItems in
+                self?.vibes = returnedItems
+            }
+            .store(in: &cancellables)
+    }
+}
